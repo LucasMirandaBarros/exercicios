f14eb6a2df02dc8e34a0971a33597e05593d9ef5
LucasMiranda <lucaswespras@gmail.com>
2023-03-25 21:17:40 -0300

    Identificador funcionando

--- src/main/java/com/segundoexercico/segundoexercicio/Controller/PrimeiroController.java
+++ src/main/java/com/segundoexercico/segundoexercicio/Controller/PrimeiroController.java
@@ -1,4 +1,22 @@
 package com.segundoexercico.segundoexercicio.Controller;
 
+import com.segundoexercico.segundoexercicio.Service.PrimeiroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/primeiro-controller")
 public class PrimeiroController {
+
+    @Autowired
+    private PrimeiroService primeiroService;
+
+    @GetMapping("/identificador")
+    public String identificador(@RequestParam("numero")Integer numero){
+        return primeiroService.identificadorDeNumero(numero);
+
+    }
 }
--- src/main/java/com/segundoexercico/segundoexercicio/Service/PrimeiroService.java
+++ src/main/java/com/segundoexercico/segundoexercicio/Service/PrimeiroService.java
@@ -5,7 +5,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class PrimeiroService {
 
-    public void primeiro(){
+    public String identificadorDeNumero(Integer numero){
+
+        if (numero > 0) {
+            return "Numero positivo";
+        } else if (numero < 0) {
+            return "Numero negativo";
+        } else {
+            return "O numero é 0";
+        }
 
     }
 }
